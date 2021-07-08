7457c7ad5f0d6c009b54b272e3a4f5f21da76ad1
Abdurr05 <abdurr2005@gmail.com>
2021-07-08 12:57:24 +0300

    first commit

---
+++ aragon_slave/src/aragon_slave.ino
@@ -0,0 +1,52 @@
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+char tx[1];
+char rx[1];
+bool CS = false;
+void takeAction(uint8_t state) {
+  if (state) {
+     CS = true; // watch for this variable in loop and set it back to false once you detect it.
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+pinMode(D5,OUTPUT);
+pinMode(D7,INPUT);
+
+SPI1.setBitOrder(MSBFIRST);
+SPI1.setDataMode(SPI_MODE0);
+SPI1.onSelect(takeAction);
+SPI1.begin(SPI_MODE_SLAVE, D6);
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  if(CS == true){
+    SPI1.transfer(tx, rx, 1, NULL);
+    if (rx[0] == '1'){
+      digitalWrite(D5,HIGH);
+    }
+    if(rx[0] == '0'){
+      digitalWrite(D5,LOW);
+    }
+    if(rx[0] == '?'){
+      if(digitalRead(D7)== 1){
+        tx[0] = '1';
+      } else{
+        tx[0] = '0';
+      }
+    }
+    CS = false;
+  }
+  // if (rx == '1'){
+  //   digitalWrite(D5,HIGH);
+  // }
+
+  
+
+  // The core of your code will likely live here.
+
+}
